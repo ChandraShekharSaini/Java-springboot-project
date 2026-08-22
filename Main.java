4d0f451a3835389a0ef002255abf6bf75b1e59aa
Chandra Shekhar Saini <chandrashekharsaini322@gmail.com>
2026-08-22 17:52:07 +0530

    Remove duplicate variable declaration for num

--- Main.java
+++ Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         var num= 10;
+          var num= 10;
+          var num= 10;
+          var num= 10;
 
 
-            var num= 10;
+          
 
 
         ;
